f4b69daf76458360f9dbdf49979e95e6bdbc52b3
start <92899739+Start-P@users.noreply.github.com>
2023-08-27 01:58:13 +0900

    Create bot.py

---
+++ bot/bot.py
@@ -0,0 +1,14 @@
+import requests
+import disnake
+from disnake.ext import commands
+import os
+
+TOKEN = "TOKEN"
+intents = disnake.Intents.all()
+
+sync_cmd = commands.CommandSyncFlags(sync_commands_debug=True)
+bot = commands.InteractionBot(intents=intents,command_sync_flags=sync_cmd)
+
+if __name__ == "__main__":
+    bot.load_extension("Genshin.GenshinCog")
+    bot.run(TOKEN)
